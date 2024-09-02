7a92f2c12f5776a8c6f03666cdf530a8491f6109
sexxOyxin <90915214+sexxOyxin@users.noreply.github.com>
2024-09-02 18:19:02 +0800

    2024-09-02_查看文件信息

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,45 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as fs from 'fs';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "play-plugin" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('play-plugin.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		const message = 'Hello World from play-plugin!';
-		vscode.window.showInformationMessage(message);
+	// shift + ctrl + p 输入helloworld
+	const disposable = vscode.commands.registerCommand('helloWorld', () => {
+		vscode.window.showInformationMessage('Hello World from play-plugin!');
+		vscode.window.showInformationMessage('Hello World from play-plugin!',{ modal: true });
 	});
 
-	context.subscriptions.push(disposable);
+    // 资源管理器和编辑器右键查看文件信息
+    const commandOfgetFileSize = vscode.commands.registerCommand('getFileSize', uri => {
+        // 文件路径
+        const filePath = uri.path.substring(1);
+        fs.stat(filePath, (err, stats) => {
+            if (err) {
+                vscode.window.showErrorMessage(`获取文件时遇到错误了${err}!!!`);
+            }
+
+            if (stats.isDirectory()) {
+                vscode.window.showWarningMessage(`检测的是文件夹，不是文件，请重新选择！！！`);
+            }
+
+            if (stats.isFile()) {
+                const size = stats.size;
+                const createTime = stats.birthtime.toLocaleString();
+                const modifyTime = stats.mtime.toLocaleString();
+                vscode.window.showInformationMessage(`
+	    文件大小为: ${size}字节
+    文件创建时间为: ${createTime}
+    文件修改时间为: ${modifyTime}
+                `, { modal: true });
+            }
+        });
+
+        const stats = fs.statSync(filePath);
+        console.log('stats', stats);
+        console.log('isFile', stats.isFile());
+    });
+
+    // 将命令放入其上下文对象中，使其生效
+    context.subscriptions.push(disposable, commandOfgetFileSize);
 }
 
 // This method is called when your extension is deactivated
